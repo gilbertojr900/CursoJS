156134de59d7ff136897d54dd0a9c23adef97bbe
Gilberto Menegon Júnior <99845776+gilbertojr900@users.noreply.github.com>
2022-04-19 13:20:23 -0300

    aula 14

---
+++ Aula14ex/modelo/script.js
@@ -0,0 +1,20 @@
+function tabuada(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if(num.value == 0){ //controle quando não se digita
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        //Limpar area para proxima tabuada
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}` //qual item foi selecionado(php)
+            tab.appendChild(item)
+            c++ 
+        }
+    }
+    
+}
